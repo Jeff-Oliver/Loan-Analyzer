12ee8320a32aebd480fc7b92086de8c8263200ea
Jeff-Oliver <jeff.b.oliver@gmail.com>
2025-11-23 13:04:42 -0500

    switched from streamlit to dash

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
-import streamlit as st
-import matplotlib.pyplot as plt
+# Import Dependencies
+from dash import Dash, html, dcc
+import plotly.express as px
 import pandas as pd
 
-# Build the Streamlit app
-st.title("Loan Payment Calculator")
-st.write("Calculate your monthly loan payments and visualize the payment breakdown.")
+# Initialize the Dash app
+app = Dash()
 
-# Input fields
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Loan Analyzer'),
+
+    html.Div(children='''
+        Dash: A web application to analyze loans.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
